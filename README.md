4ab5d725542ee7a22221bdd4d51b37dddc895501
Angela <angelacr@ymail.com>
2021-03-07 15:15:01 -0600

    fixing cards for five day forecast

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # how-is-the-weather
+
+As a user I want to be able to search a city and get a 5 day forecast.
--- assets/JS/script.js
+++ assets/JS/script.js
@@ -3,3 +3,9 @@ var temp = document.querySelector("#temp")
 var humidity = document.querySelector("#humidity")
 var wind = document.querySelector("#wind")
 var uv = document.querySelector("#uv")
+
+
+//when i look up a city the search button will display the weather info
+//get icons to display what type of weather is shown
+//call on the api
+//
